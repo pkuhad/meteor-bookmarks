2f60533b27fedd4cceccc978ea501821494ec3ee
Paras Kuhad <paras.kuhad@changer.nl>
2013-11-02 22:14:46 +0530

    Form interacting with end database store

--- meteor-share-resource.js
+++ meteor-share-resource.js
@@ -4,6 +4,26 @@ if (Meteor.isClient) {
   Template.resource_container.resources = function () {
     return Resources.find({});
   };
+
+  Template.resource_form.events({
+    'click .resource-submit': function (evt, tmpl) { 
+      var resource_url = document.getElementById('resource_url');
+      var resource_description = document.getElementById('resource_description');
+      if (resource_url.value != '') {
+        Resources.insert({
+          url: resource_url.value,
+          description: resource_description.value,
+          time: Date.now(),
+        });
+
+        document.getElementById('resource_url').value = '';
+        document.getElementById('resource_description').value = '';
+        resource_url.value = '';
+        resource_description.value = '';
+      }
+
+    }
+  });
 }
 
 if (Meteor.isServer) {
@@ -14,7 +34,7 @@ if (Meteor.isServer) {
                    "http://example-1.com",
                    "http://example-2.com"];
       for (var i = 0; i < urls.length; i++)
-        Resources.insert({url: urls[i]});
+        Resources.insert({url: urls[i], time: Date.now()});
     }
   });
 }
