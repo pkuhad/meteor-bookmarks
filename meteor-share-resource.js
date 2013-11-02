5384c4ad74d54f97f639b70576645abe862a55ce
Paras Kuhad <paras.kuhad@changer.nl>
2013-11-02 21:49:52 +0530

    Resource logic

--- meteor-share-resource.js
+++ meteor-share-resource.js
@@ -1,19 +1,21 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to meteor-share-resource.";
-  };
+Resources = new Meteor.Collection("resources");
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+if (Meteor.isClient) {
+  Template.resource_container.resources = function () {
+    return Resources.find({});
+  };
+}
+
+if (Meteor.isServer) {
+  // Bootstrap data
+  Meteor.startup(function () {
+    if (Resources.find().count() === 0) {
+      var urls = [ "http://example.com/",
+                   "http://example-1.com",
+                   "http://example-2.com"];
+      for (var i = 0; i < urls.length; i++)
+        Resources.insert({url: urls[i]});
     }
   });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
